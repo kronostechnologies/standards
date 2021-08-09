54de0a8e16d33d68e1cf40d941aa87c2531edd09
Christopher Viel <christopher.viel@equisoft.com>
2021-08-09 10:44:04 -0400

    chore(kotlin): update Gradle wrapper to 7.1.1

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -123,6 +123,8 @@ tasks {
 
     wrapper {
         distributionType = Wrapper.DistributionType.ALL
+        distributionSha256Sum = "9bb8bc05f562f2d42bdf1ba8db62f6b6fa1c3bf6c392228802cc7cb0578fe7e0"
+        gradleVersion = "7.1.1"
     }
 }
 
