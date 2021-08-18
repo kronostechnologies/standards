09a72f2c6ca03204f16c6e1d82caf8233488fbe8
Christopher Viel <christopher.viel@equisoft.com>
2021-08-18 16:27:40 -0400

    chore(gradle/conventions): bump to 0.0.7-SNAPSHOT

--- gradle/conventions/build.gradle.kts
+++ gradle/conventions/build.gradle.kts
@@ -2,7 +2,7 @@ group = "com.equisoft.standards"
 
 subprojects {
     group = "com.equisoft.standards"
-    version = "0.0.6"
+    version = "0.0.7-SNAPSHOT"
 
     apply(plugin = "maven-publish")
 
