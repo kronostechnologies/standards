e733466b905519dc4264bbd532071937114a1fa3
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2021-11-30 09:55:31 -0500

    Update micronaut generator

--- gradle/openapi-sdk/build.gradle.kts
+++ gradle/openapi-sdk/build.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
 
     compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     implementation("org.openapitools:openapi-generator-gradle-plugin:$openApiVersion")
-    implementation("com.equisoft.openapi.generator.micronaut:micronaut-project-openapi-generator:0.2.1")
+    implementation("com.equisoft.openapi.generator.micronaut:micronaut-project-openapi-generator:0.3.0")
 
     openApiGenerator("org.openapitools:openapi-generator:$openApiVersion")
 }
