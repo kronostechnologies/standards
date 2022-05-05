09fe7eaa311228961a71d97fe97232a246a2b3f1
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2022-05-05 17:27:25 -0400

    feat(global-conventions): remove license and dependency check plugins

--- gradle/global-conventions/src/main/kotlin/com/equisoft/standards/gradle/global/GlobalConventionsPlugin.kt
+++ gradle/global-conventions/src/main/kotlin/com/equisoft/standards/gradle/global/GlobalConventionsPlugin.kt
@@ -2,9 +2,6 @@ package com.equisoft.standards.gradle.global
 
 import com.github.benmanes.gradle.versions.VersionsPlugin
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-import com.github.jk1.license.LicenseReportExtension
-import com.github.jk1.license.LicenseReportPlugin
-import com.github.jk1.license.render.InventoryHtmlReportRenderer
 import net.linguica.gradle.maven.settings.MavenSettingsPlugin
 import org.cyclonedx.gradle.CycloneDxPlugin
 import org.gradle.api.Plugin
@@ -13,18 +10,12 @@ import org.gradle.kotlin.dsl.invoke
 import org.gradle.kotlin.dsl.maven
 import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.repositories
-import org.owasp.dependencycheck.gradle.DependencyCheckPlugin
-import org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension
-import org.owasp.dependencycheck.reporting.ReportGenerator.Format
-import java.net.URL
 
 @Suppress("LongMethod")
 class GlobalConventionsPlugin : Plugin<Project> {
     override fun apply(project: Project): Unit = with(project) {
         plugins.apply(VersionsPlugin::class.java)
         plugins.apply(MavenSettingsPlugin::class.java)
-        plugins.apply(DependencyCheckPlugin::class.java)
-        plugins.apply(LicenseReportPlugin::class.java)
         plugins.apply(CycloneDxPlugin::class.java)
 
         repositories {
@@ -38,27 +29,6 @@ class GlobalConventionsPlugin : Plugin<Project> {
                         ?: System.getenv("GPR_KEY")
                         ?: System.getenv("GHCR_TOKEN")
                 }
-            }
-        }
-
-        afterEvaluate {
-            extensions.configure(DependencyCheckExtension::class.java) {
-                formats = listOf(Format.SARIF, Format.HTML)
-
-                suppressionFiles = listOf(
-                    OWASP_COMMON_SUPPRESSIONS_FILE,
-                    "owasp-suppressions.xml"
-                )
-            }
-
-            extensions.configure(LicenseReportExtension::class.java) {
-                allowedLicensesFile = URL(ALLOWED_LICENSES_FILE)
-                renderers = arrayOf(
-                    InventoryHtmlReportRenderer("license-inventory-report.html"),
-                )
-                excludeBoms = true
-                excludeOwnGroup = true
-                excludeGroups = arrayOf("com.equisoft.*")
             }
         }
 
@@ -79,12 +49,5 @@ class GlobalConventionsPlugin : Plugin<Project> {
                 }
             }
         }
-    }
-
-    companion object {
-        private const val OWASP_COMMON_SUPPRESSIONS_FILE =
-            "https://raw.githubusercontent.com/kronostechnologies/standards/master/gradle/owasp-suppressions.xml"
-        private const val ALLOWED_LICENSES_FILE =
-            "https://raw.githubusercontent.com/kronostechnologies/standards/master/gradle/allowed-licenses.json"
     }
 }
