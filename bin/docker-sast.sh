dd135a007c30514a04e61481661e29c2fcaa5afc
Maxime Mériouma-Caron <meriouma@users.noreply.github.com>
2022-09-15 14:18:04 -0400

    chore(docker-sast): replace Dockle with Trivy

--- bin/docker-sast.sh
+++ bin/docker-sast.sh
@@ -43,8 +43,7 @@ touch "$output_file"
 output_file_path=$(realpath "$output_file")
 
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
-  -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
   -v "$output_file_path":/tmp/output.sarif \
   --user "$(id -u):$(id -g)" \
-  goodwithtech/dockle:v0.4.6 \
-  -f sarif -o /tmp/output.sarif "$target_image"
+  aquasec/trivy:0.31.3 \
+  --cache-dir /tmp/.cache image -f sarif -o /tmp/output.sarif "$target_image"
