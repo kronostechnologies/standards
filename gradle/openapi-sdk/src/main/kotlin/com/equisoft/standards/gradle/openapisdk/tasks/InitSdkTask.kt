1331657b83ba17e10b8178cc8dfd3095b4276e9d
Christian Arcand <christian.arcand@equisoft.com>
2023-05-10 14:43:58 -0400

    Init sdk repository Task

--- gradle/openapi-sdk/src/main/kotlin/com/equisoft/standards/gradle/openapisdk/tasks/InitSdkTask.kt
+++ gradle/openapi-sdk/src/main/kotlin/com/equisoft/standards/gradle/openapisdk/tasks/InitSdkTask.kt
@@ -1,5 +1,6 @@
 package com.equisoft.standards.gradle.openapisdk.tasks
 
+import com.equisoft.standards.gradle.openapisdk.createOutput
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.OutputDirectory
@@ -7,11 +8,35 @@ import org.gradle.api.tasks.TaskAction
 
 @CacheableTask
 abstract class InitSdkTask : GitTask() {
+
     @get:OutputDirectory
     abstract val directory: DirectoryProperty
 
     @TaskAction
     fun initSdk() {
+        val directory = directory.get().asFile
+        val output = createOutput()
+
+        val sdkRepositoryExists = project
+            .exec(directory, "git", "ls-remote", "-h", uri.get(), directory.path, displayResult = true)
+            .line()
+            .first{
+                it.trim().startsWith("ERROR: Repository not found.")
+            }
+        val directoryCreated = directory.mkdirs().isBlank();
+
+        if (!sdkRepositoryExists) {
+            if (directoryCreated) {
+                output.style(Normal).println("Create $sdkRepository")
+                directory.project.exec(directory, "git", "init", "-b", "master", "--template", "")
+            } else {
+                // directory already exists on filesystem but there is no repo on github. What do we do?
+            }
+        } else {
+            project.delete(directory)
+            directory.mkdirs()
+            project.exec("git", "clone", uri.get(), directory.path)
+        }
         /*
            ## Init
            1. Create GH repository. If exists: goto update
