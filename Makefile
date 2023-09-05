5e717a266e71ee9efa2c2ecaaae48b26c1e9e57d
Christopher Viel <Chris-V@users.noreply.github.com>
2023-09-05 13:16:36 -0400

    Clean up deprecated renovate configs (#199)
    J'ai aussi fait un cleanup de certains scripts

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 BASE_DIR := $(dir $(realpath $(firstword $(MAKEFILE_LIST))))
 
+# renovate: datasource=npm depName=renovate
+RENOVATE_VERSION := 36.81.0
+
 setup_asdf = ./bin/setup_asdf.bash
 exit_error = (>&2 echo -e ">> \x1B[31m$1\x1B[39m" && exit 1)
 
@@ -8,7 +11,15 @@ setup:
 	@$(setup_asdf)
 
 .PHONY: check
-check:
+check: check.workflows check.renovate
+
+.PHONY: check.workflows
+check.workflows:
+	@actionlint -color
+
+.PHONY: check.renovate
+check.renovate:
+	@npx --package="renovate@${RENOVATE_VERSION}" -- renovate-config-validator --strict renovate-base.json renovate-config.json
 
 .PHONY: test
 test:
@@ -26,7 +37,3 @@ all: setup check test compile package
 .PHONY: publish-javascript
 publish-javascript:
 	cd javascript && yarn workspaces foreach -tv --include '@equisoft/*' npm publish --access public --tag next
-
-.PHONY: scan_secrets
-scan_secrets:
-	@./bin/gitleaks.bash gitleaks.toml
--- bin/gitleaks.bash
+++ bin/gitleaks.bash
@@ -1,56 +1,5 @@
 #!/usr/bin/env bash
 
-# This file is read by external tools. Do not move.
-# Usage example in external projects:
-#  curl -sSL https://raw.githubusercontent.com/kronostechnologies/standards/master/bin/gitleaks.bash | bash
+echo "Github secret scan is enable on all repositories. To fix this error remove usages of gitleaks from your project."
 
-set -e
-
-BUILD_OUTPUT=build/gitleaks
-CONTAINER_PATH=/repo
-COMMITS_FILE=$BUILD_OUTPUT/commits.txt
-OPTIONS=("--verbose" "--source=$CONTAINER_PATH")
-REMOTE_CONFIG=https://raw.githubusercontent.com/kronostechnologies/standards/master/gitleaks.toml
-
-rm -rf $BUILD_OUTPUT
-mkdir -p $BUILD_OUTPUT
-
-scan () {
-    docker run --rm \
-        -u "$(id -u):$(id -g)" \
-        -v "$(pwd):$CONTAINER_PATH" \
-        zricethezav/gitleaks:v8.0.2 \
-        "$@"
-}
-
-if [ -f "$1" ]; then
-    cp -f "$1" "$BUILD_OUTPUT/gitleaks.toml"
-else
-    [[ -n "$1" ]] && echo "$1 is not a file"
-    curl -s "$REMOTE_CONFIG" -o "$BUILD_OUTPUT/gitleaks.toml"
-fi
-OPTIONS+=("--config=$CONTAINER_PATH/$BUILD_OUTPUT/gitleaks.toml")
-
-if [[ -n "${CI:-}" ]]; then
-    OPTIONS+=("--redact")
-
-    if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
-        base_branch="origin/$GITHUB_BASE_REF"
-    fi
-else
-    base_branch="origin/$(git remote show origin | grep 'HEAD branch' | cut -d' ' -f5)"
-fi
-
-commits_count=all
-if [ "$base_branch" != "" ]; then
-    commits_count="$(git rev-list --count remotes/$base_branch...)"
-    if [ "$commits_count" = "0" ]; then
-        echo "No commits to scan. Exiting."
-        exit 0
-    fi
-
-    OPTIONS+=("--log-opts=--left-right --cherry-pick --pretty=format:\"%H\" remotes/$base_branch...")
-fi
-
-echo "Scanning $commits_count commits"
-scan detect --report-format sarif --report-path "$BUILD_OUTPUT/gitleaks.sarif" "${OPTIONS[@]}"
+exit 1
